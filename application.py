aeb1f6286b1dd670658fded8181ab3ceb72c18f9
buptwjy <1215549420@qq.com>
2020-05-28 20:23:11 +0800

    框架调整

--- application.py
+++ application.py
@@ -11,71 +11,16 @@ class Room:#房间的基类
 
     def Change_tem(self):#环境温度改变
 
-
-class User:#客户的基类
-    def __init__(self,user_id,room_id,b_time,e_time ):
-        self.user_id =user_id#客户号
-        self.room_id = room_id#入住房间号
-        self.b_time=b_time#入住时间
-        self.e_time=e_time#退房时间
-
-    def login(self):#入住，登录系统
-    def air_on(self):#启动空调
-    def air_off(self):#关闭空调
-    def change_wind(self):#调节风速
-    def change_tem(self):#调节温度
-    def logout(self):#退房,退出系统
-
-
-class Air_admin:  #空调管理员的基类
-    def __init__(self, admin_id, if_login, status):
-        self.admin_id = admin_id#管理员编号
-        self.if_login = if_login#是否登录
-        self.status = status#身份标识
-
-    def login(self):  # 登录系统
-    def power_on(self):  #初始化空调系统
-    def init_air(self):  # 初始化空调参数
-    def print_hotel(self):  # 查看酒店空调运行状态
-    def print_room(self):  # 查看房间空调运行状态
-    def logout(self):  # 退出系统
-
-
-class Cashier:  # 酒店前台的基类
-    def __init__(self, cashier_id, if_login, status):
-        self.cashier_id = cashier_id#酒店前台编号
-        self.if_login = if_login#是否登录
-        self.status = status#身份标识
-
-    def login(self):  # 登录系统
-    def create_bill(self):  # 创建账单
-    def print_bill(self):  # 查看账单
-    def logout(self):  # 退出系统
-
-
-class Manager:  # 酒店经理的基类
-    def __init__(self, manager_id, if_login, status):
-        self.manager_id = manager_id#经理编号
-        self.if_login = if_login#是否登录
-        self.status = status#身份标识
-
-    def login(self):  #登录系统
-    def create_form(self): #创建报表
-    def print_form(self):  #查看报表
-    def logout(self):  #退出系统
-
-
 class Air_main:  # 空调主机的基类
     def __init__(self, air_on_num, wind_on_num, login_mode):
-        self.air_on_num = air_on_num  # 开启子机数
-        self.wind_on_num = wind_on_num  # 送风子机数
-        self.login_mode = login_mode  # 登录模式
+        self.air_on_num = air_on_num #开启子机数
+        self.wind_on_num = wind_on_num #送风子机数
+        self.login_mode = login_mode #登录模式
 
     def create_service(self):  # 创建服务对象
     def create_scheduler(self):  # 创建调度对象
     def create_air_sub(self):  # 创建空调子机对象
     def check_has(self):  # 查看酒店空调状态
-
 
 class Air_sub:  # 空调子机的基类_
     def __init__(self, room_id, tem, wind_mode, cost, if_wind, if_on):
